77368918e518a708bfad52eb3722d50e5804de8d
mahyar naeimi <mn.mahyar.naeimi@gmail.com>
2025-05-22 12:29:41 -0400

    updates

---
+++ js/button.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('button')
+
+let ageIn = document.getElementById('age')
+
+button.onclick = function () {
+    if(ageIn.value >= 18){
+        alert('Welcome to our portial')
+    }
+    if(ageIn.value < 18){
+        alert('You are not allowed to log in')
+    }
+}
