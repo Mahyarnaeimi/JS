dde1c9add44165375b6c93caf96c98aa1bea518c
test <mn.mahyar.naeimi@gmail.com>
2025-05-27 11:21:31 -0400

    update

---
+++ js/temp.js
@@ -0,0 +1,6 @@
+oldArr = [1, 2, 3, 4, 5];
+
+const newArr = oldArr.map(function(currentValue, index, array) {
+  // Do something with currentValue
+  return currentValue * 2; // Example operation
+});
