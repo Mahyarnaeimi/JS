850fa702efd4394850a076dc446616de727ecb97
test <mn.mahyar.naeimi@gmail.com>
2025-06-17 11:03:57 -0400

    today

--- js/temp.js
+++ js/temp.js
@@ -1,8 +1,25 @@
-for (let i = 1; i <= 5; i++) {
-  let row = "";
-  for (let j = 1; j <= i; j++) {
-    row += "* ";
+class Car {
+  constructor(make, model, year) {
+    // Encapsulated data
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.isEngineRunning = false;
+    this.currentSpeed = 0;
   }
-  console.log(row);
-}
 
+  // Encapsulated methods
+  startEngine() {
+    this.isEngineRunning = true;
+    console.log('Engine started.');
+  }
+
+  accelerate(speed) {
+    if (this.isEngineRunning) {
+      this.currentSpeed += speed;
+      console.log(`Accelerated to ${this.currentSpeed} km/h.`);
+    } else {
+      console.log('Engine is not running. Please start the engine first.');
+    }
+  }
+}
