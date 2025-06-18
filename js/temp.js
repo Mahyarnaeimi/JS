ec7aee91feebb566d141701f8b2706cba4bb2e6a
test <mn.mahyar.naeimi@gmail.com>
2025-06-18 11:16:07 -0400

    new

--- js/temp.js
+++ js/temp.js
@@ -1,10 +1,72 @@
-const characters = ["Goku", "Chi-Chi","Vegeta", "Bulma", "Krillin", "Android 18", "Yamcha"];
-
-const addIsCool = characters.map(name => name + " is cool");
-const longNamedChars = characters.filter(name => name.length > 6);
-const bulmasIndex = characters.findIndex(name => name === "Bulma");
-const indexOfName = (nameToFind) => {
-	return characters.findIndex(name => {
-		return name === nameToFind
-	});
+class Item {
+	constructor (name, description, damage = 0) {
+		this.name = name;
+		this.description = description;
+		this.damage = damage
+	}
 }
+
+class Character {
+	constructor (name, playerName, hitPoints, charLevel = 1) {
+		this.campaign = "The Pirate Campaign"
+		this.name = name;
+		this["player name"] = playerName;
+		this.hp = hitPoints;
+		this.maxHp = hitPoints;
+		this.level = charLevel;
+		this.hpPerLevel = 6;
+		this.inventory = [];
+	}
+	levelUp () {
+		this.level += 1;
+		this.hp += this.hpPerLevel;
+		this.maxHp += this.hpPerLevel;
+	}
+	addItemToInventory (itemToAdd) {
+		if (itemToAdd instanceof Item === false) {
+			console.log("Error - addItemToInventory must receive an Item")
+		} else {
+			this.inventory.push(itemToAdd)
+		}
+	}
+	takeDamage (damage = 0) {
+		this.hp = Math.max(0, (this.hp - damage))
+	}
+}
+
+class Cleric extends Character {
+	constructor(name, playerName, hitPoints, charLevel) {
+    super(name, playerName, hitPoints, charLevel);
+		this.class = "Cleric";
+		this.hpPerLevel = 8;
+  }
+	heal(target) {
+		if (target instanceof Character === false) {
+			return console.log("target to heal must be a Character")
+		}
+		const random = Math.random();
+		const oneToSix = Math.ceil(random*6);
+		const healValue = this.level * oneToSix;
+		target.hp += healValue;
+		target.hp = Math.min(target.hp, target.maxHp);
+		console.log(`${this.name} heals ${target.name} for up to ${healValue} life.`)
+	}
+}
+
+const drud = new Character("Drud", "Bobba", 70, 5);
+const eep = new Character("Eep", "Scott", 15);
+const marlin = new Cleric("Marlin", "Matt", 27, 3);
+
+console.log(drud, eep, marlin);
+
+console.log("drud goes up a level, picks up a sword and takes 50 damage");
+
+drud.addItemToInventory(new Item("Sword", "A long one-handed sword", 8))
+drud.levelUp();
+drud.takeDamage(50)
+
+console.log(drud)
+
+marlin.heal(drud)
+
+console.log(drud)
